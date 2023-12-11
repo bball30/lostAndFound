b405076e5bdfc6d6f42d572e99c335a5402745c9
Maksim <maksinismile@gmail.com>
2023-12-11 18:32:49 +0300

    Initial commit

---
+++ src/test/java/ru/itmo/lostandfound/LostAndFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.lostandfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
